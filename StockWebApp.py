01ff8e5df0432327474b09cb7a378d49c8c0ceac
ThuPhuong2042 <77660134+ThuPhuong2042@users.noreply.github.com>
2021-02-07 21:55:44 +0700

    Update StockWebApp.py

--- StockWebApp.py
+++ StockWebApp.py
@@ -5,6 +5,7 @@ import streamlit as st
 from bs4 import BeautifulSoup
 import csv
 import urllib.request
+from python_graphql_client import GraphqlClient
 import requests, time
 import json
 import pandas as pd
@@ -13,41 +14,181 @@ import plotly.express as px
 import plotly.graph_objects as go
 
 
-
 #Load the data
-outfile = open("ckhoan.csv","w", newline='',encoding='utf-8')
-writer = csv.writer(outfile)
+# Instantiate the client with an endpoint.
+client = GraphqlClient(endpoint="https://gateway-iboard.ssi.com.vn/graphql")
 
-webUrl = urllib.request.urlopen('https://raw.githubusercontent.com/ThuPhuong2042/project/main/22.html')
-source_code = webUrl.read()
-#query = pyquery(source_code)
+# Create the query string and variables required for the request.
+query = """
+    query stockRealtimes($exchange: String) {
+  stockRealtimes(exchange: $exchange) {
+    stockNo
+    ceiling
+    floor
+    refPrice
+    stockSymbol
+    stockType
+    exchange
+    matchedPrice
+    matchedVolume
+    priceChange
+    priceChangePercent
+    highest
+    avgPrice
+    lowest
+    nmTotalTradedQty
+    best1Bid
+    best1BidVol
+    best2Bid
+    best2BidVol
+    best3Bid
+    best3BidVol
+    best4Bid
+    best4BidVol
+    best5Bid
+    best5BidVol
+    best6Bid
+    best6BidVol
+    best7Bid
+    best7BidVol
+    best8Bid
+    best8BidVol
+    best9Bid
+    best9BidVol
+    best10Bid
+    best10BidVol
+    best1Offer
+    best1OfferVol
+    best2Offer
+    best2OfferVol
+    best3Offer
+    best3OfferVol
+    best4Offer
+    best4OfferVol
+    best5Offer
+    best5OfferVol
+    best6Offer
+    best6OfferVol
+    best7Offer
+    best7OfferVol
+    best8Offer
+    best8OfferVol
+    best9Offer
+    best9OfferVol
+    best10Offer
+    best10OfferVol
+    buyForeignQtty
+    buyForeignValue
+    sellForeignQtty
+    sellForeignValue
+    caStatus
+    tradingStatus
+    currentBidQty
+    currentOfferQty
+    remainForeignQtty
+    session
+    __typename
+  }
+}
 
-soup = BeautifulSoup(source_code)
-#table_tag = soup.find_all('table',{'class' : 'price table-scroll-table'})
-table = soup.find('table',{'class' : 'price table-scroll-table'})
-list_of_rows = []
-for row in table.find_all('tr'):
-    list_of_cells = []
-    for cell in row.find_all(["th","td"]):
-        text = cell.text
-        list_of_cells.append(text)
-    list_of_rows.append(list_of_cells)
+"""
+variables = {"exchange": "hose"}
 
+# Synchronous request
+data = client.execute(query=query, variables=variables)
+# print(data)  #=> {'data': {'stockRealtimes': {'stockNo': 'hose:21', 'stockSymbol': "AAA"}}}
+# df = pd.DataFrame.from_dict(data['data']['stockRealtimes'])
+# print(df)
+# df.to_csv('test.csv', index=False)
 
+header_list = []
+raw_data = data['data']['stockRealtimes']
 
-col_Names=["CK", "Tran", "San", "TC",
-           "Giamua3","KLmua3","Giamua2","KLmua2","Giamua1","KLmua1",
-           "Giakhop","Khoiluongkhoplenh","Thaydoi","Phantramthaydoi",
-           "Giaban3","KLban3","Giaban2","KLban2","Giaban1","KLban1"
-           "Caonhat","Thapnhat","TongKL",
-           "DTNNmua","DTNNban","DTNNdu"]
-writer.writerow(col_Names)
+header_list.append('stockSymbol')
+header_list.append('ceiling')
+header_list.append('floor')
+header_list.append('refPrice')
+header_list.append('best3Bid')
+header_list.append('best3BidVol')
+header_list.append('best2Bid')
+header_list.append('best2BidVol')
+header_list.append('best1Bid')
+header_list.append('best1BidVol')
+header_list.append('matchedPrice')
+header_list.append('matchedVolume')
+header_list.append('priceChange')
+header_list.append('priceChangePercent')
+header_list.append('best1Offer')
+header_list.append('best1OfferVol')
+header_list.append('best2Offer')
+header_list.append('best2OfferVol')
+header_list.append('best3Offer')
+header_list.append('best3OfferVol')
+header_list.append('highest')
+# header_list.append('avgPrice')
+header_list.append('lowest')
+header_list.append('nmTotalTradedQty')
+header_list.append('buyForeignQtty')
+header_list.append('sellForeignQtty')
+header_list.append('remainForeignQtty')
 
-df = pd.read_csv("ckhoan.csv",names=col_Names)
+data_file = open('ckhoan.csv', 'w', newline='')
 
-for item in list_of_rows:
-     writer.writerow(item)
-     print(' '.join(item))
+# create the csv writer object
+csv_writer = csv.writer(data_file)
+col_Names = ["CK", "Tran", "San", "TC",
+             "Giamua3", "KLmua3", "Giamua2", "KLmua2", "Giamua1", "KLmua1",
+             "Giakhop", "Khoiluongkhoplenh", "Thaydoi", "Phantramthaydoi",
+             "Giaban3", "KLban3", "Giaban2", "KLban2", "Giaban1", "KLban1"
+                                                                  "Caonhat", "Thapnhat", "TongKL",
+             "DTNNmua", "DTNNban", "DTNNdu"]
+
+# for emp in data:
+count = 0
+list_of_Allcells = []
+for j in range(0, len(raw_data)):
+    data_list = []
+    if count == 0:
+        csv_writer.writerow(col_Names)
+
+        count += 1
+    for i in range(0, len(header_list)):
+        r = raw_data[j][header_list[i]]
+        if ('priceChange' in header_list[i]):
+            if (r != ''):
+                if (header_list[i] == 'priceChange'):
+                    r = '{:.2f}'.format(float(r) / 1000)
+                else:
+                    r = '{:.1f}'.format(float(r))
+        else:
+            r = r
+        if (type(r) == int or type(r) == float):
+            # b = header_list[i]
+            #  if ('priceChangePercent' in header_list[i]):
+            #        r = r/100000
+            if ('Vol' in header_list[i] or 'Qt' in header_list[i] or r == 0):
+                r = r
+            else:
+                r = r / 1000
+            a = str(r)
+        elif r == None:
+            a = str(0)
+        else:
+            a = r
+        data_list.append(a)
+    list_of_Allcells.append(data_list)
+
+    # Writing data of CSV file
+for item in list_of_Allcells:
+    csv_writer.writerow(item)
+
+data_file.close()
+
+# Asynchronous request
+# import asyncio
+
+# data = asyncio.run(client.execute_async(query=query, variables=variables))
+# print(data)  # => {'data': {'country': {'code': 'CA', 'name': 'Canada'}}}
 
 df = pd.read_csv("ckhoan.csv",names=col_Names)
 df = df.drop(0)
@@ -76,10 +217,11 @@ for i in range(1, 14):
         writer.writerow(item)
 
 outfile.close()
-
 df1 = pd.read_csv("table.csv", names = col_names)
 df1 = df1.drop(0).drop(columns=['ID', 'ISIN', "FIGI"])
 hose = df1.merge(df)
+
+
 #Convert
 hose['Giakhop'] = hose['Giakhop'].astype(float)
 hose['Thaydoi'] = hose['Thaydoi'].astype(float)

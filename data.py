f4a6358247a7daf213ca1c28684e5f22a8bcbea2
ThuPhuong2042 <77660134+ThuPhuong2042@users.noreply.github.com>
2021-03-24 13:53:00 +0700

    Update data.py

--- data.py
+++ data.py
@@ -2,5 +2,5 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-dataframe = np.random.randn(10, 20)
-st.dataframe(dataframe)
+data = pd.read_csv('https://raw.githubusercontent.com/ThuPhuong2042/project/main/datangtruong.csv')
+st.table(dataframe)

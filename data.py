92495337f349b835f1c486e863544448489a1692
ThuPhuong2042 <77660134+ThuPhuong2042@users.noreply.github.com>
2021-03-24 10:27:02 +0700

    Update data.py

--- data.py
+++ data.py
@@ -1,5 +1,5 @@
 import streamlit as st
 import pandas as pd
 
-data = pd.read_csv('C:/Users/ThuPhuong/PycharmProjects/dictionaryweb/datangtruong.csv')
+data = pd.read_csv('https://raw.githubusercontent.com/ThuPhuong2042/project/main/datangtruong.csv')
 st.write(data)

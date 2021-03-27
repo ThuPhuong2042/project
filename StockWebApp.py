b93b3d484bc4798f1b3224977b5d7a1b42b771fb
ThuPhuong2042 <77660134+ThuPhuong2042@users.noreply.github.com>
2021-03-27 16:24:52 +0700

    Update StockWebApp.py

--- StockWebApp.py
+++ StockWebApp.py
@@ -1,11 +1,10 @@
 #Description: This is a stock market dashboard to show some charts and data on some stock
 
 #Import the libraries
-import streamlit as st
+import streamlit as streamlit
 from bs4 import BeautifulSoup
 import csv
 import urllib.request
-from python_graphql_client import GraphqlClient
 import requests, time
 import json
 import pandas as pd
@@ -13,186 +12,44 @@ import numpy as np
 import plotly.express as px
 import plotly.graph_objects as go
 import datetime
-from plotly.subplots import make_subplots
 
 #Add a title
-st.title('Stock Web')
+streamlit.title('Khuyến nghị giao dịch cổ phiếu')
 #Load the data
-# Instantiate the client with an endpoint.
-client = GraphqlClient(endpoint="https://gateway-iboard.ssi.com.vn/graphql")
+outfile = open("ckhoan.csv","w", newline='',encoding='utf-8')
+writer = csv.writer(outfile)
 
-# Create the query string and variables required for the request.
-query = """
-    query stockRealtimes($exchange: String) {
-  stockRealtimes(exchange: $exchange) {
-    stockNo
-    ceiling
-    floor
-    refPrice
-    stockSymbol
-    stockType
-    exchange
-    matchedPrice
-    matchedVolume
-    priceChange
-    priceChangePercent
-    highest
-    avgPrice
-    lowest
-    nmTotalTradedQty
-    best1Bid
-    best1BidVol
-    best2Bid
-    best2BidVol
-    best3Bid
-    best3BidVol
-    best4Bid
-    best4BidVol
-    best5Bid
-    best5BidVol
-    best6Bid
-    best6BidVol
-    best7Bid
-    best7BidVol
-    best8Bid
-    best8BidVol
-    best9Bid
-    best9BidVol
-    best10Bid
-    best10BidVol
-    best1Offer
-    best1OfferVol
-    best2Offer
-    best2OfferVol
-    best3Offer
-    best3OfferVol
-    best4Offer
-    best4OfferVol
-    best5Offer
-    best5OfferVol
-    best6Offer
-    best6OfferVol
-    best7Offer
-    best7OfferVol
-    best8Offer
-    best8OfferVol
-    best9Offer
-    best9OfferVol
-    best10Offer
-    best10OfferVol
-    buyForeignQtty
-    buyForeignValue
-    sellForeignQtty
-    sellForeignValue
-    caStatus
-    tradingStatus
-    currentBidQty
-    currentOfferQty
-    remainForeignQtty
-    session
-    __typename
-  }
-}
+webUrl = urllib.request.urlopen('https://raw.githubusercontent.com/ThuPhuong2042/project/main/27_3_2021.html')
+source_code = webUrl.read()
 
-"""
-variables = {"exchange": "hose"}
+#query = pyquery(source_code)
 
-# Synchronous request
-data = client.execute(query=query, variables=variables)
-# print(data)  #=> {'data': {'stockRealtimes': {'stockNo': 'hose:21', 'stockSymbol': "AAA"}}}
-# df = pd.DataFrame.from_dict(data['data']['stockRealtimes'])
-# print(df)
-# df.to_csv('test.csv', index=False)
+soup = BeautifulSoup(source_code)
+#table_tag = soup.find_all('table',{'class' : 'price table-scroll-table'})
+table = soup.find('table',{'class' : 'price table-scroll-table'})
+list_of_rows = []
+for row in table.find_all('tr'):
+    list_of_cells = []
+    for cell in row.find_all(["th","td"]):
+        text = cell.text
+        list_of_cells.append(text)
+    list_of_rows.append(list_of_cells)
 
-header_list = []
-raw_data = data['data']['stockRealtimes']
 
-header_list.append('stockSymbol')
-header_list.append('ceiling')
-header_list.append('floor')
-header_list.append('refPrice')
-header_list.append('best3Bid')
-header_list.append('best3BidVol')
-header_list.append('best2Bid')
-header_list.append('best2BidVol')
-header_list.append('best1Bid')
-header_list.append('best1BidVol')
-header_list.append('matchedPrice')
-header_list.append('matchedVolume')
-header_list.append('priceChange')
-header_list.append('priceChangePercent')
-header_list.append('best1Offer')
-header_list.append('best1OfferVol')
-header_list.append('best2Offer')
-header_list.append('best2OfferVol')
-header_list.append('best3Offer')
-header_list.append('best3OfferVol')
-header_list.append('highest')
-# header_list.append('avgPrice')
-header_list.append('lowest')
-header_list.append('nmTotalTradedQty')
-header_list.append('buyForeignQtty')
-header_list.append('sellForeignQtty')
-header_list.append('remainForeignQtty')
 
-data_file = open('ckhoan.csv', 'w', newline='')
+col_Names=["CK", "Tran", "San", "TC",
+           "Giamua3","KLmua3","Giamua2","KLmua2","Giamua1","KLmua1",
+           "Giakhop","Khoiluongkhoplenh","Thaydoi","Phantramthaydoi",
+           "Giaban3","KLban3","Giaban2","KLban2","Giaban1","KLban1"
+           "Caonhat","Thapnhat","TongKL",
+           "DTNNmua","DTNNban","DTNNdu"]
+writer.writerow(col_Names)
 
-# create the csv writer object
-csv_writer = csv.writer(data_file)
-col_Names = ["CK", "Tran", "San", "TC",
-             "Giamua3", "KLmua3", "Giamua2", "KLmua2", "Giamua1", "KLmua1",
-             "Giakhop", "Khoiluongkhoplenh", "Thaydoi", "Phantramthaydoi",
-             "Giaban3", "KLban3", "Giaban2", "KLban2", "Giaban1", "KLban1"
-                                                                  "Caonhat", "Thapnhat", "TongKL",
-             "DTNNmua", "DTNNban", "DTNNdu"]
-
-# for emp in data:
-count = 0
-list_of_Allcells = []
-for j in range(0, len(raw_data)):
-    data_list = []
-    if count == 0:
-        csv_writer.writerow(col_Names)
-
-        count += 1
-    for i in range(0, len(header_list)):
-        r = raw_data[j][header_list[i]]
-        if ('priceChange' in header_list[i]):
-            if (r != ''):
-                if (header_list[i] == 'priceChange'):
-                    r = '{:.2f}'.format(float(r) / 1000)
-                else:
-                    r = '{:.1f}'.format(float(r))
-        else:
-            r = r
-        if (type(r) == int or type(r) == float):
-            # b = header_list[i]
-            #  if ('priceChangePercent' in header_list[i]):
-            #        r = r/100000
-            if ('Vol' in header_list[i] or 'Qt' in header_list[i] or r == 0):
-                r = r
-            else:
-                r = r / 1000
-            a = str(r)
-        elif r == None:
-            a = str(0)
-        else:
-            a = r
-        data_list.append(a)
-    list_of_Allcells.append(data_list)
-
-    # Writing data of CSV file
-for item in list_of_Allcells:
-    csv_writer.writerow(item)
-
-data_file.close()
-
-# Asynchronous request
-# import asyncio
-
-# data = asyncio.run(client.execute_async(query=query, variables=variables))
-# print(data)  # => {'data': {'country': {'code': 'CA', 'name': 'Canada'}}}
-
+df = pd.read_csv("ckhoan.csv",names=col_Names)
+df.head()
+for item in list_of_rows:
+     writer.writerow(item)
+     print(' '.join(item))
 df = pd.read_csv("ckhoan.csv",names=col_Names)
 df = df.drop(0)
 c = 0
@@ -266,8 +123,7 @@ dandatplot = dandatplot.add_trace(go.Bar(x=dandat.index, y=dandat['DIEMANHHUONGA
 dandatplot.update_layout(title_text='Nhóm dẫn dắt thị trường')
 
 
-#list khuyen nghi
-
+#List khuyen nghi
 stock = pd.read_csv('https://raw.githubusercontent.com/ThuPhuong2042/project/main/stock_file.csv')
 
 for i in range(1,len(stock['Date'])) :
@@ -278,9 +134,12 @@ stock['Date'] = pd.to_datetime(stock.Date)
 
 stock = stock.sort_values(by='Date',ascending = True)
 stock = stock.set_index('Date')
-vni = pd.read_csv("https://raw.githubusercontent.com/ThuPhuong2042/project/main/vni_table.csv").drop(['a','b','c','d'], axis=1).apply(lambda x: x.str.replace(',','.'))
+vni = pd.read_csv("https://raw.githubusercontent.com/ThuPhuong2042/project/main/vni_table.csv").drop(
+    ['a', 'b', 'c', 'd'], axis=1).apply(lambda x: x.str.replace(',', '.'))
 vni = vni.drop(0)
+vni.head()
 vni['VNI_Close'] = vni['VNI_Close'].astype(float)
+
 for i in range(1, len(vni['Date'])):
     loop = vni['Date'][i]
     vni_Date = datetime.datetime.strptime(loop, '%d/%m/%Y')
@@ -340,33 +199,32 @@ for i in range(0, len(ma)):
         stock_return = (Tic['Percent Change'] + 1).cumprod()[-1]
         returns_multiple = round((stock_return / vni_return), 2)
     #     t.append(Tic)
-    returns_multiples.append(returns_multiple)
-    #     returns_multiple = round((stock_return / vni_return), 2)
-    # stock_return
-    # for s in t:
-    #     if t['Percent Change'] == 'AAA':
-    #          stock_return = (t['Percent Change'] + 1).cumprod()[-1]
-    # stock_return
 
-    # stock_return
-    # returns_multiple
+    returns_multiples.append(returns_multiple)
+#     returns_multiple = round((stock_return / vni_return), 2)
+# stock_return
+# for s in t:
+#     if t['Percent Change'] == 'AAA':
+#          stock_return = (t['Percent Change'] + 1).cumprod()[-1]
+# stock_return
+
+# stock_return
+# returns_multiple
 rs_df = pd.DataFrame(list(zip(ma, returns_multiples)), columns=['Ticker', 'Returns_multiple'])
 rs_df['RS_Rating'] = rs_df.Returns_multiple.rank(pct=True) * 100
 rs_df = rs_df[rs_df.RS_Rating >= rs_df.RS_Rating.quantile(.70)]
 rs_df = rs_df.sort_values(by='RS_Rating', ascending=False)
-
-
 # Checking Minervini conditions of top 30% of stocks in given list
 exportList = pd.DataFrame(
     columns=['MÃ CỔ PHIẾU', 'CHỈ SỐ RS', 'SMA50', 'SMA150', 'SMA200', 'ĐÁY 52 TUẦN', 'ĐỈNH 52 TUẦN'])
 rs_stocks = rs_df['Ticker']
-for stocki in rs_stocks:
+for st in rs_stocks:
     try:
         #         rs_sto = []
         #         for sto in stock:
         #             if sto["Ticker"] == st:
         #                 rs_sto.append(sto)
-        df = stock[stock["Ticker"] == stocki]
+        df = stock[stock["Ticker"] == st]
         sma = [50, 150, 200]
         for x in sma:
             df["SMA_" + str(x)] = round(df['Adj.Close'].rolling(window=x).mean(), 2)
@@ -382,7 +240,7 @@ for stocki in rs_stocks:
         moving_average_200 = df["SMA_200"][-1]
         low_of_52week = round(min(df["Low"][-260:]), 2)
         high_of_52week = round(max(df["High"][-260:]), 2)
-        RS_Rating = round(rs_df[rs_df['Ticker'] == stocki].RS_Rating.tolist()[0])
+        RS_Rating = round(rs_df[rs_df['Ticker'] == st].RS_Rating.tolist()[0])
 
         try:
             moving_average_200_20 = df["SMA_200"][-20]
@@ -420,32 +278,32 @@ for stocki in rs_stocks:
         if (
                 condition_1 and condition_2 and condition_3 and condition_4 and condition_5 and condition_6 and condition_7 and condition_8 and condition_9):
             exportList = exportList.append(
-                {'MÃ CỔ PHIẾU': stocki, 'CHỈ SỐ RS': RS_Rating, 'SMA50': moving_average_50, 'SMA150': moving_average_150
+                {'MÃ CỔ PHIẾU': st, 'CHỈ SỐ RS': RS_Rating, 'SMA50': moving_average_50, 'SMA150': moving_average_150
                     , 'SMA200': moving_average_200, 'ĐÁY 52 TUẦN': low_of_52week, 'ĐỈNH 52 TUẦN': high_of_52week},
                 ignore_index=True)
-            print(stocki + " đạt điểm mua")
     except Exception as e:
         print(e)
-        print(f"Could not gather data on {stocki}")
+        print(f"Could not gather data on {st}")
 # currentClose
 exportList = exportList.sort_values(by='CHỈ SỐ RS', ascending=False)
-print('\n', exportList)
 # writer = ExcelWriter("ScreenOutput.xlsx")
 # exportList.to_excel(writer, "Sheet1")
 # writer.save()
 
+
 #List xu huong
-exportList2 = pd.DataFrame(columns=['MÃ CỔ PHIẾU','GIÁ ĐÓNG CỬA' ,'CHỈ SỐ RS', 'SMA50', 'SMA150', 'SMA200', 'ĐÁY 52 TUẦN', 'ĐỈNH 52 TUẦN'])
+exportList2 = pd.DataFrame(
+    columns=['MÃ CỔ PHIẾU', 'GIÁ ĐÓNG CỬA', 'CHỈ SỐ RS', 'SMA50', 'SMA150', 'SMA200', 'ĐÁY 52 TUẦN', 'ĐỈNH 52 TUẦN'])
 rs_stocks = rs_df['Ticker']
-for stock2 in rs_stocks:    
+for st in rs_stocks:
     try:
 
-        df = stock[stock["Ticker"] == stock2]
+        df = stock[stock["Ticker"] == st]
         sma = [50, 150, 200]
         for x in sma:
-            df["SMA_"+str(x)] = round(df['Adj.Close'].rolling(window=x).mean(), 2)
-        
-        # Storing required values 
+            df["SMA_" + str(x)] = round(df['Adj.Close'].rolling(window=x).mean(), 2)
+
+        # Storing required values
         currentClose = df["Adj.Close"][-1]
         Pre_Close = df["Adj.Close"][-2]
         currentOpen = df["Open"][-1]
@@ -457,8 +315,8 @@ for stock2 in rs_stocks:
         moving_average_200 = df["SMA_200"][-1]
         low_of_52week = round(min(df["Low"][-260:]), 2)
         high_of_52week = round(max(df["High"][-260:]), 2)
-        RS_Rating = round(rs_df[rs_df['Ticker']== stock2].RS_Rating.tolist()[0])
-        
+        RS_Rating = round(rs_df[rs_df['Ticker'] == st].RS_Rating.tolist()[0])
+
         try:
             moving_average_200_20 = df["SMA_200"][-20]
         except Exception:
@@ -466,46 +324,48 @@ for stock2 in rs_stocks:
 
         # Condition 1: Current Price > 150 SMA and > 200 SMA
         condition_1 = currentClose > moving_average_150 > moving_average_200
-        
+
         # Condition 2: 150 SMA and > 200 SMA
         condition_2 = moving_average_150 > moving_average_200
 
         # Condition 3: 200 SMA trending up for at least 1 month
         condition_3 = moving_average_200 > moving_average_200_20
-        
+
         # Condition 4: 50 SMA> 150 SMA and 50 SMA> 200 SMA
         condition_4 = moving_average_50 > moving_average_150 > moving_average_200
-           
+
         # Condition 5: Current Price > 50 SMA
         condition_5 = currentClose > moving_average_50
-           
+
         # Condition 6: Current Price is at least 30% above 52 week low
-        condition_6 = currentClose >= (1.3*low_of_52week)
-           
+        condition_6 = currentClose >= (1.3 * low_of_52week)
+
         # Condition 7: Current Price is within 25% of 52 week high
-        condition_7 = currentClose >= (.75*high_of_52week)
-        
-        
-        
-#         If all conditions above are true, add stock to exportList
-        if(condition_1 and condition_2 and condition_3 and condition_4 and condition_5 and condition_6 and condition_7 ):
-            exportList2 = exportList2.append({'MÃ CỔ PHIẾU': stock2,'GIÁ ĐÓNG CỬA' : currentClose ,'CHỈ SỐ RS': RS_Rating ,'SMA50': moving_average_50, 'SMA150': moving_average_150
-                                            , 'SMA200': moving_average_200, 'ĐÁY 52 TUẦN': low_of_52week, 'ĐỈNH 52 TUẦN': high_of_52week}, ignore_index=True)
+        condition_7 = currentClose >= (.75 * high_of_52week)
+
+        #         If all conditions above are true, add stock to exportList
+        if (
+                condition_1 and condition_2 and condition_3 and condition_4 and condition_5 and condition_6 and condition_7):
+            exportList2 = exportList2.append(
+                {'MÃ CỔ PHIẾU': st, 'GIÁ ĐÓNG CỬA': currentClose, 'CHỈ SỐ RS': RS_Rating, 'SMA50': moving_average_50,
+                 'SMA150': moving_average_150
+                    , 'SMA200': moving_average_200, 'ĐÁY 52 TUẦN': low_of_52week, 'ĐỈNH 52 TUẦN': high_of_52week},
+                ignore_index=True)
     except Exception as e:
-        print (e)
-        print(f"Could not gather data on {stock2}")
+        print(e)
+        print(f"Could not gather data on {st}")
 # currentClose
 exportList2 = exportList.sort_values(by='CHỈ SỐ RS', ascending=False)
-exportList2.to_csv('exportList2.csv')
-print('\n', exportList2)
+
+
 
 #Display
-st.markdown('Tổng quan thị trường')
-st.plotly_chart(vonhoaplot)
-st.plotly_chart(dandatplot)
+streamlit.markdown('Tổng quan thị trường')
+streamlit.plotly_chart(vonhoaplot)
+streamlit.plotly_chart(dandatplot)
 
 #Print list cp co xu huong tang
-st.markdown('Danh sách cổ phiếu khuyến nghị mua')
-st.dataframe(exportList)
-st.markdown('Danh sách cổ phiếu có xu hướng tăng')
-st.dataframe(exportList2)
+streamlit.markdown('Danh sách cổ phiếu khuyến nghị mua')
+streamlit.dataframe(exportList)
+streamlit.markdown('Danh sách cổ phiếu có xu hướng tăng')
+streamlit.dataframe(exportList2)
